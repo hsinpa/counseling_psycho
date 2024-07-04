e64d53ed1eef1cab8993c5bd890854f5cca7d716
Hsinpa <hsinpa@gmail.com>
2024-07-05 00:05:24 +0800

    [Cognitive behavior] Organize data before post fetch

--- app/pages/cognitive_behavior/cognitive_behavior.tsx
+++ app/pages/cognitive_behavior/cognitive_behavior.tsx
@@ -28,31 +28,48 @@ let RenderSingleSliderSlot = function(question: QuestionFormType) {
 
 export let CognitiveBehaviorView = function({questions}: {questions: QuestionFormType[]}) {
 
+    let on_submit = function() {
+        let all_inputs = document.querySelectorAll('textarea, input[type="range"]');
+        console.log(all_inputs);
+        
+        let post_content: any[] = []
+
+        all_inputs.forEach((x: any) =>{
+            let item = questions.find(x=>x.id == x.id);
+
+            if (item == null || x.value == '') return;
+            
+            post_content.push({
+                questionnaire_id: item.id,
+                questionnaire: item.content,
+                content: x.value,    
+            });
+        });
+
+        console.log(post_content)
+    }
+    
+
     return (<div className="container cognitive_behavior_container">
         <RenderShortTheoryForm title={'理論二 認知行為療法'}></RenderShortTheoryForm>
         <RenderLongCognitiveForm questions={questions}></RenderLongCognitiveForm>
 
-        <button className='button is-fullwidth is-info is-light'>獲得個案分析報告</button>
+        <button className='button is-fullwidth is-info is-light' onClick={on_submit}>獲得個案分析報告</button>
     </div>);
 }
 
 export let RenderLongCognitiveForm = function({questions}: {questions: QuestionFormType[]}) {
-
-    let all_questions: JSX.Element[] = [];
     if (questions == null) questions = [];
 
-    for (let i = 0; i < questions.length; i++) {
-        if (questions[i].type == 'text_field')
-            all_questions.push(RenderSingleQuestionSlot(questions[i]));
-
-        if (questions[i].type == 'number')
-            all_questions.push(RenderSingleSliderSlot(questions[i]))
-    }
-    
+    let text_fields = questions.filter(x => x.type == 'text_field').map(x=>RenderSingleQuestionSlot(x));
+    let numbers = questions.filter(x => x.type == 'number').map(x=>RenderSingleSliderSlot(x));
 
     return (
         <div className='cognitive_long_questions'>
-            {all_questions}
+            {text_fields}
+            <br></br>
+            <p>最後想邀請您做一個小小的問卷，請您仔細回想在最近一星期中(包括今天), 這些問題使您感到困擾或苦惱的程度, 分別給予0-4分的分數, 0分是完全沒有, 4分是非常厲害</p>
+            {numbers}
         </div>
     );
 }

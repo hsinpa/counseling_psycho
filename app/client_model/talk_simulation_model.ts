dfafe6af2b2be8b68b37f15153cb19745764bf71
Hsinpa <hsinpa@gmail.com>
2024-10-07 16:47:58 +0800

    UI for talk simulation not done

---
+++ app/client_model/talk_simulation_model.ts
@@ -0,0 +1,52 @@
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+type TalkSimulationState = {
+    age: number,
+    gender: string,
+    job: string,
+    education: string,
+    theme_checkboxes: string[],
+
+    theme_reason: string,
+    sorting_reason: string,
+
+    set_age: (age: number) => void,
+    set_gender: (gender: string) => void,
+    set_education: (education: string) => void,
+    set_job: (job: string) => void,
+
+    set_checkboxes: (boxes: string[]) => void,
+    set_theme_reason: (reason: string) => void,
+    set_sorting_reason: (reason: string) => void,
+}
+
+export const useaTalkSimulationStore = create<TalkSimulationState>()(
+    immer((set) => ({
+        age: 6,
+        gender: 'male',
+        job: '',
+        education: 'university',
+        theme_checkboxes: [],
+        theme_reason: '',
+        sorting_reason: '',
+
+        set_age: (age: number) => set((state) => {state.age = age } ),
+        set_gender: (gender: string) => set((state) => { state.gender = gender } ),
+        set_education: (education: string) => set((state) => {state.education = education } ),
+        set_job: (job: string) => set((state) => { state.job =job } ),
+
+        set_checkboxes: (boxes: string[]) => set((state) => {
+            state.theme_checkboxes = boxes;
+        }),
+
+        set_theme_reason: (reason: string) => set((state) => {
+            state.theme_reason = reason;
+        }),
+
+        set_sorting_reason: (reason: string) => set((state) => {
+            state.sorting_reason = reason;
+        }),
+        
+    })),
+)
--- app/pages/talk_simulation/talk_simulation_input.tsx
+++ app/pages/talk_simulation/talk_simulation_input.tsx
@@ -3,14 +3,24 @@ import './talk_simulation.scss'
 import { SimulationThemeCheckboxType } from './talk_simulation_type'
 import { useLoaderData } from '@remix-run/react'
 import { useEffect, useState } from 'react'
+import { useaTalkSimulationStore } from '~/client_model/talk_simulation_model'
 
 
 export let RenderTalkSimulationInputUI = function({checkboxes}: {checkboxes: SimulationThemeCheckboxType[]}) {
+    let set_theme_reason = useaTalkSimulationStore(s=>s.set_theme_reason);
+    let set_sorting_reason = useaTalkSimulationStore(s=>s.set_sorting_reason);
+    let sorting_reason = useaTalkSimulationStore(s=>s.sorting_reason);
+    let theme_reason = useaTalkSimulationStore(s=>s.theme_reason);
+
     return (
         <div className="simulation_input_container">
             <div className="simulation_input_row">
                 <RenderBasicInfoTextForm></RenderBasicInfoTextForm>
                 <RenderBasicThemeCheckbox checkboxes={checkboxes}></RenderBasicThemeCheckbox>
+            </div>
+            <div className="simulation_input_row">
+                 <RenderTextarea title='選擇這些主題的原因:' textarea_val={theme_reason} on_textarea_change={set_theme_reason} ></RenderTextarea>
+                 <RenderTextarea title='這樣排序的原因:' textarea_val={sorting_reason} on_textarea_change={set_sorting_reason} ></RenderTextarea>
             </div>
         </div>
     )
@@ -18,6 +28,8 @@ export let RenderTalkSimulationInputUI = function({checkboxes}: {checkboxes: Sim
 
 
 let RenderBasicInfoTextForm = function() {
+    const simulationStore = useaTalkSimulationStore();
+
     return (
     <div className="simulation_input_info_text">
         <h2 className='title'>基本資料</h2>
@@ -25,33 +37,38 @@ let RenderBasicInfoTextForm = function() {
         <section>
             <div className="field">
                 <label className="label">您的年齡</label>
-                <input className="input" name="age" type='number' placeholder="年齡"></input>
+                <input className="input" name="age" type='number' placeholder="年齡" value={simulationStore.age}
+                onChange={x=>simulationStore.set_age(Number.parseInt(x .target.value))}></input>
             </div>
 
             <div className="field">
                 <label className="label">性別</label>
                 <div className="select">
-                    <select name='gender'>
-                        <option value='Male'>男</option>
-                        <option value='Female'>女</option>
-                        <option value='Both'>其他</option>
+                    <select name='gender' value={simulationStore.gender} 
+                    onChange={x=>simulationStore.set_gender(x.target.value)}>
+                        <option value='male' >男</option>
+                        <option value='female' >女</option>
+                        <option value='both' >其他</option>
                     </select>
                 </div>
             </div>
 
             <div className="field">
                 <label className="label">職業</label>
-                <input className="input" name="theme" type='text' placeholder="輸入主題"></input>
+                <input className="input" name="theme" type='text' placeholder="輸入職業" value={simulationStore.job}
+                onChange={x=>simulationStore.set_job(x.target.value)}></input>
             </div>
 
             <div className="field">
                 <label className="label">最高學歷</label>
                 <div className="select">
-                    <select name='gender'>
-                        <option>無</option>
-                        <option>高中</option>
-                        <option>大學</option>
-                        <option>研究所以上</option>
+                    <select name='gender' 
+                    value={simulationStore.education}
+                    onChange={x=> simulationStore.set_education(x.target.value)}>
+                        <option value={'university'} >大學</option>
+                        <option value={'high_school'} >高中</option>
+                        <option value={'master_or_above'} >研究所以上</option>
+                        <option value={'below_high_school'} >高中以下</option>
                     </select>
                 </div>
             </div>
@@ -60,12 +77,35 @@ let RenderBasicInfoTextForm = function() {
 }
 
 let RenderBasicThemeCheckbox = function({checkboxes}: {checkboxes: SimulationThemeCheckboxType[]}) {
-    
+    const MAX_SELECTION = 3;
+    const set_selected_boxes = useaTalkSimulationStore(s=>s.set_checkboxes);
+    const selected_boxes = useaTalkSimulationStore(s=>s.theme_checkboxes);
+
+    let on_checkbox_change = function(e: React.FormEvent<HTMLInputElement>) {
+        let target: HTMLInputElement = e.target as (HTMLInputElement);
+
+        if (target.checked && selected_boxes.length + 1 > MAX_SELECTION) {
+            e.preventDefault();
+            return;
+        };
+        
+        if (!target.checked) {
+            set_selected_boxes(selected_boxes.filter(x=>x!=target.id));
+        } else {
+            set_selected_boxes([...selected_boxes, target.id]);
+        }
+    }
+
     let theme_checkboxes = [];
-    for (let checkbox of checkboxes) {
+    for (let i = 0; i < checkboxes.length; i++) {
+        let checkbox = checkboxes[i];
+        let is_checked = selected_boxes.findIndex(x=>x == checkbox.id) >= 0;
+        let is_disable = selected_boxes.length >= MAX_SELECTION && !is_checked;
+
         theme_checkboxes.push(
             <div key={checkbox.id} className='checkbox_item'>
-                <input type="checkbox" id={checkbox.id} name={checkbox.id} />
+                <input type="checkbox" id={checkbox.id} name={checkbox.id} onChange={on_checkbox_change}
+                checked={is_checked} disabled={is_disable}/>
                 <label htmlFor={checkbox.id}>{checkbox.ch_name}</label> 
             </div>
         )
@@ -84,4 +124,14 @@ let RenderBasicThemeCheckbox = function({checkboxes}: {checkboxes: SimulationThe
             </fieldset>
         </div>
     );    
+}
+
+let RenderTextarea = function({title, textarea_val, on_textarea_change}: {
+    title: string, textarea_val: string, on_textarea_change: (text_input: string) => void} ) {
+        
+    return (
+    <div className='simulation_textarea'>
+        <h3>{title}</h3>
+        <textarea onChange={x => on_textarea_change(x.target.value)}value={textarea_val}></textarea>
+    </div>);
 }
